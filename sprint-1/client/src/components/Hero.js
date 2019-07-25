361f1a3ee29ebcdc00f97ebcb57e8ac942fad77b
Michael Ryan <mcryan@shaw.ca>
2019-07-24 23:03:10 -0700

    Video controls for now

--- sprint-1/client/src/components/Hero.js
+++ sprint-1/client/src/components/Hero.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import Video from "../assets/images/video-list-0.jpg"
+import Play from "../assets/icons/SVG/Icon-play.svg"
+import Scrubber from "../assets/icons/SVG/Icon-scrubber-control.svg"
+import FullScreen from "../assets/icons/SVG/Icon-fullscreen.svg"
+import Volume from "../assets/icons/SVG/Icon-volume.svg"
 
 export default function Hero(){
     return (
-        <section className="hero-section">
-            <video controls poster={Video}></video>
-        </section>
+        <div className="hero-section">
+            <video className="video" poster={Video}></video>
+            <div className="control">
+                <div className="control__icons control__play">
+                    <img src={Play} alt="play"/>
+                </div>
+                <div className="control__icons control__scrubber">
+                    <img src={Scrubber} alt="play" />
+                </div>
+                <div className="control__icons control__screen-vol">
+                  
+                        <img className="screen-icon" src={FullScreen} alt="play" />
+                                  
+                        <img className="vol-icon" src={Volume} alt="play" />
+
+                </div>
+            </div>
+        </div>
     )
 }
