5754c28a7f6a5d5281ee8df9ecb39be31ad95068
Mike Ryan <mryan@SURFACEPRO.leighfs3.local>
2019-07-25 16:09:55 -0700

    Completed static sections

--- sprint-1/client/src/components/About.js
+++ sprint-1/client/src/components/About.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import "../styles/partials/about"
+import Views from "../assets/icons/SVG/Icon-views.svg"
+import Likes from "../assets/icons/SVG/Icon-likes.svg"
 
 export default function About() {
     return (
-        <div>
-            
+        <div className="about">
+            <div className="about__header">
+                <h1 className="about__title">BMX Rampage: 2018 Highlights</h1>
+                <div className="about__subtitle">
+                    <div className="about__subtitle--left">
+                        <div className="author">By Red Cow</div>
+                        <h4 className="date">12/18/2018</h4>
+                    </div>
+                    <div className="about__subtitle--right">
+                        <div className="views">
+                            <img src={Views}/><span>1,001,023</span>
+                        </div>
+                        <div className="likes">
+                            <img src={Likes} /><span>110,985</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="about__body">
+                <h4 className="about__body--copy">
+                    On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his ﬁrst Red Cow Rampage title
+                </h4>
+            </div>
         </div>
     )
 }
--- sprint-1/client/src/components/Comments.js
+++ sprint-1/client/src/components/Comments.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import "../styles/partials/comments"
 
 export default function Comments() {
     return (
