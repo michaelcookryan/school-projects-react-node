b6c74d0664ebb47e1cfa4102a8e89216de925b72
Mike Ryan <mryan@SURFACEPRO.leighfs3.local>
2019-07-26 18:41:20 -0700

    corrected components' handling

--- sprint-1/client/src/components/About.js
+++ sprint-1/client/src/components/About.js
@@ -3,22 +3,69 @@ import Views from "../assets/icons/SVG/Icon-views.svg"
 import Likes from "../assets/icons/SVG/Icon-likes.svg"
 import AllComments from './AllComments';
 
-export default function About() {
+class About extends React.Component{
+
+    state = {
+    mainVideo: {
+            id: 'type of <string>',
+            title: 'BMX Rampage: 2018 Highlights',
+            description: 'On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his ﬁrst Red Cow Rampage title',
+            channel: 'By Red Cow',
+            image: 'type of <string>',
+            views: '1,001,023',
+            likes: '110,985',
+            duration: 'type of <string>',
+            video: 'type of <string>',
+            timestamp: '12/18/2018',
+            comments: [
+                {
+                    name: 'Theodore Duncan',
+                    date: '11/15/2018',
+                    commentCopy: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever!'
+                },
+
+                {
+                    name: 'Gary Wong',
+                    date: '12/12/2018',
+                    commentCopy: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
+                },
+
+                {
+                    name: 'Michael Lyons',
+                    date: '12/18/2018',
+                    commentCopy: 'They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
+                }
+            ]
+        }
+    }
+
+    addComment = comment => {
+        
+        let addedToList = this.state.mainVideo.comments
+        addedToList.unshift(comment)
+        this.setState({
+            comments: addedToList
+        })
+    }
+  
+    render(){
+
     return (
         <div className="about">
+            
             <div className="about__header">
-                <h1 className="about__title">BMX Rampage: 2018 Highlights</h1>
+                <h1 className="about__title">{this.state.mainVideo.title}</h1>
                 <div className="about__subtitle">
                     <div className="about__subtitle--left">
-                        <h4 className="author">By Red Cow</h4>
-                        <h4 className="date">12/18/2018</h4>
+                        <h4 className="channel">{this.state.mainVideo.channel}</h4>
+                        <h4 className="date">{this.state.mainVideo.timestamp}</h4>
                     </div>
                     <div className="about__subtitle--right">
                         <div className="views">
-                            <img src={Views} alt="views-icon"/><span>1,001,023</span>
+                            <img src={Views} alt="views-icon"/><span>{this.state.mainVideo.views}</span>
                         </div>
                         <div className="likes">
-                            <img src={Likes} alt="likes-icon"/><span>110,985</span>
+                            <img src={Likes} alt="likes-icon" /><span>{this.state.mainVideo.likes}</span>
                         </div>
                     </div>
                 </div>
@@ -26,10 +73,16 @@ export default function About() {
 
             <div className="about__body">
                 <h4 className="about__body--copy">
-                    On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his ﬁrst Red Cow Rampage title
+                    {this.state.mainVideo.description}
                 </h4>
             </div>
-            <AllComments/>
+
+            <AllComments comments={this.state.mainVideo.comments} addComment={this.addComment}/>
+
         </div>
     )
+    }
+   
 }
+
+export default About
--- sprint-1/client/src/components/AllComments.js
+++ sprint-1/client/src/components/AllComments.js
@@ -1,63 +1,32 @@
 import React from 'react'
 import Avitar from "../assets/images/Mohan-muruge.jpg";
 
-class AllComments extends React.Component {
-
-    state = {
-        comments : [
-            {
-            name:'Theodore Duncan',
-            date: '11/15/2018',
-            commentCopy: 'How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s deﬁnitely my favorite ever!'
-            },
-
-            {
-            name: 'Gary Wong',
-            date: '12/12/2018',
-            commentCopy: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!'
-            },
-
-            {
-            name: 'Michael Lyons',
-            date: '12/18/2018',
-            commentCopy: 'They BLEW the ROOF off at their last show, once everyone started ﬁguring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.'
-            }
-        ]
-    } 
+function AllComments(props){
 
 
-
-addComment = comment =>{
-    let addedToList = this.state.comments
-    addedToList.unshift(comment)
-
-        this.setState({
-            comments: addedToList
-        })
-}
-
-
-    render(){
+    //render(){
         return (
             <div className="comments">
                 <div className="comments-wrapper__form-wrap">
-                        <div className="comments-wrapper__image">
-                            <img src={Avitar} alt="avitar"/>
-                        </div>
-
-                        <NewComments addComment={this.addComment}/>
-
+                       
+                    <div className="comments-wrapper__image">
+                        <img src={Avitar} alt="avitar"/>
                     </div>
 
-                <CommentsList comments={this.state.comments}/>
-                
+                    <NewComments addComment={props.addComment}/>
+
+                </div>
+
+            <CommentsList comments={props.comments}/>
+
             </div>
         )
-    }
+    //}
 }
 
 
 function NewComments(props) {
+
     const handleSubmit = event => {
         event.preventDefault()
 
@@ -66,11 +35,13 @@ function NewComments(props) {
             date: "today",
             commentCopy: event.target.commentCopy.value
         }
+
         props.addComment(newComment)
        
-        event.target.reset();
+        event.target.reset()
 
     }
+
     return (
         <form className="comments__form" onSubmit={handleSubmit}>
             <label className="comment-label">
@@ -83,21 +54,27 @@ function NewComments(props) {
 }
 
 function CommentsList(props) {
+
     const commentList = props.comments.map(comment => {
+
         return <li className="comments__list--item">
+                    
                     <div className="avitar">
                         <img src={Avitar} alt="avitar" />
                     </div>
+
                     <div className="details">
                         <div className="details__header">
                             <h4 className="name">{comment.name}</h4>
                             <h4 className="date">{comment.date}</h4>
                             {/* <h4 className="since-posted">' + timeSincePosted(commentObject.timestamp) + '</h4> */}
-                </div></div>
+                        </div>
+                    </div>
                         <h4 className="comment-copy">{comment.commentCopy}</h4>
-                </li>;
-    });
-    return <ul className="comments__list">{commentList}</ul>;
+                </li>
+    })
+
+    return <ul className="comments__list">{commentList}</ul>
 }
 
 export default AllComments;
