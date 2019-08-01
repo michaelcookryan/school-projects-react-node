05082223a9ce6e1ed5c2b675f798ababc39b74f6
Mike Ryan <mryan@SURFACEPRO.leighfs3.local>
2019-07-31 17:10:06 -0700

    Videos are populating

--- sprint-2/client/src/components/App.js
+++ sprint-2/client/src/components/App.js
@@ -3,28 +3,12 @@ import "../styles/app.css";
 import Header from './Header';
 import Home from './Home';
 import Upload from './Upload';
-import axios from 'axios';
 import {Route, Switch} from 'react-router-dom';
-const api_url = 'https://project-2-api.herokuapp.com/videos';
-const my_key = '?api_key=4f6764a2-4a25-45a8-90b7-d7e52c6890f8'; 
 
 class App extends React.Component {
 
-  state = {
-    videos:[],
-    comments:[]
-  }
-
-
-  componentDidMount(){
-    axios.get(api_url + my_key)
-    .then(response =>{
-      console.log(response.data)
-    })
-  }
-
-
   render(){
+  
     return (
       <div className="App">
         <Header />
--- sprint-2/client/src/components/Home.js
+++ sprint-2/client/src/components/Home.js
@@ -2,15 +2,40 @@ import React, { Component } from 'react'
 import Hero from './Hero';
 import VideoList from './VideoList';
 import About from './About';
+import axios from 'axios';
+const api_url = 'https://project-2-api.herokuapp.com/videos';
+const my_key = '?api_key=4f6764a2-4a25-45a8-90b7-d7e52c6890f8'; 
 
-class Home extends Component {
+
+//Might need to import axios again fro the comments!!!
+
+
+class Home extends Component{
+    state = {
+        videos: [],
+        comments: []
+    }
+
+
+    componentDidMount() {
+        axios.get(api_url + my_key)
+            .then(response => {
+                this.setState({
+                    videos: response.data
+                }, () => {
+                        console.log(response.data)
+                })
+            })
+
+    }
     render() {
+        
         return (
             <div>
                 <Hero/>
                 <main>
                     <About />
-                    <VideoList />
+                    <VideoList videos={this.state.videos}/>
                 </main>
                
             </div>
