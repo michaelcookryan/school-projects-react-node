3a56dd1d3cd6a60ea3b839f66e040e0aef008d71
Michael Ryan <mcryan@shaw.ca>
2019-08-05 20:12:03 -0700

    Final QA

--- sprint-2/client/src/components/Upload.js
+++ sprint-2/client/src/components/Upload.js
@@ -7,41 +7,40 @@ export default class Upload extends Component {
                 <main>
                     <div className="upload">
                     <h1 className="upload__title">Upload Video</h1>
+
                     <section>
+
                         <div className="upload__image-area">
                             <label>
                                 <h5>Video Thumbnail</h5>
                             </label>
-                            <img className="upload__thumbnail" src={VideoThumb} />
+                            <img className="upload__thumbnail" src={VideoThumb} alt="video-thumbnail"/>
                         </div>
                     
                        
-                    <form className="upload__form">                       
-                            <label>
-                                <h5>Title Your Video</h5>
-                            </label>
-                            <input className="upload__form--title" type="text" placeholder="Add a title to your video" required/>
+                        <form className="upload__form">  
                             
-                            <label>
-                                <h5 className="description-heading">Add a Video Description</h5>
-                            </label>
-                            <textarea className="upload__form--description" placeholder="Add a description of your video" name="descriptionCopy" required></textarea>
-                            
-                            {/* <div className="upload__form--buttons">
-                                <button className="publish-btn">Publish</button>
                                 <label>
-                                    <h5 className="clear-btn">Clear</h5>
+                                    <h5>Title Your Video</h5>
                                 </label>
-                            </div> */}
+                                <input className="upload__form--title" type="text" placeholder="Add a title to your video" required/>
+                                
+                                <label>
+                                    <h5 className="description-heading">Add a Video Description</h5>
+                                </label>
+                                <textarea className="upload__form--description" placeholder="Add a description of your video" name="descriptionCopy" required></textarea>
                             
-                    </form>
+                        </form>
+                        
                     </section>
-                    <div className="upload__buttons">
-                        <button className="publish-btn">Publish</button>
-                        <label>
-                            <h5 className="clear-btn">Clear</h5>
-                        </label>
-                    </div>
+
+                        <div className="upload__buttons">
+                            <button className="publish-btn">Publish</button>
+                            <label>
+                                <h5 className="clear-btn">Clear</h5>
+                            </label>
+                        </div>
+
                     </div>                                       
                 </main>
                
